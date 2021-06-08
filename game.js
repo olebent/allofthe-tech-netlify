ab454ea18ee01142771e068785623552b6929315
olebent <34416702+olebent@users.noreply.github.com>
2021-06-08 15:25:08 +0200

    Update game.js

--- game.js
+++ game.js
@@ -50,7 +50,7 @@ function create () {
 			bugGenLoop.destroy();
 			this.physics.pause();
 
-			this.add.text(280, 150, 'Game Over \n Klikke for å restarte', { fontSize: '15px', fill: '#000' })
+			this.add.text(280, 150, 'Game Over \n Klikk for å restarte', { fontSize: '15px', fill: '#000' })
 			gameState.score = 0
 
 			this.input.on('pointerdown', () => {
